5075dbec659bb9654faa3c30ccdc738e54517360
Anderson-reis7 <Anderson.200810@gmail.com>
2024-12-01 09:25:25 -0300

    feat: add and style sidebar component; include cover and profile images; install phosphor-react for icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { Header } from "./components/Header";
-import { Post } from "./Post"
-import './global.css';
+import { Post } from "./Post";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
-      <Header/>
-      <Post
-        autho="Anderson"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aperiam esse atque harum id. Vitae eum labore voluptatibus placeat iure, fugit quam et neque aperiam perspiciatis cupiditate totam natus incidunt."
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            autho="Anderson"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aperiam esse atque harum id. Vitae eum labore voluptatibus placeat iure, fugit quam et neque aperiam perspiciatis cupiditate totam natus incidunt."
+          />
 
-      <Post
-        autho="Camis"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aperiam esse atque harum id. Vitae eum labore voluptatibus placeat iure, fugit quam et neque aperiam perspiciatis cupiditate totam natus incidunt."
-      />
+          <Post
+            autho="Camis"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aperiam esse atque harum id. Vitae eum labore voluptatibus placeat iure, fugit quam et neque aperiam perspiciatis cupiditate totam natus incidunt."
+          />
+        </main>
+      </div>
     </div>
   );
 }
-
-
